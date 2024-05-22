1036e37f520eaa5fefbd66b09920dbba4f3f96ed
junnun15 <junnunprodhan@gmail.com>
2024-05-23 00:30:17 +0600

    add material ui

---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/material/styles';
+
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1586FD',
+    },
+    secondary: {
+      main: "#666f732",
+    },
+  },
+  components:{
+    MuiButton:{
+        defaultProps:{
+            variant:"contained",
+        },
+        styleOverrides:{
+            root:{
+                padding:"8px 24px"
+            }
+        }
+    },
+    MuiContainer:{
+        defaultProps:{
+            maxWidth:"lg"
+        }
+    }
+  },
+
+});
