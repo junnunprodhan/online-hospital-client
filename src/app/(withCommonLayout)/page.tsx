4b196d872bb40be88a56b2ec4fd1ca28a2df4e4d
junnun15 <junnunprodhan@gmail.com>
2024-05-30 01:06:35 +0600

    add specialist

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 
 import HeroSection from '@/components/UI/HomePage/HeroSection/HeroSection';
+import Specialist from '@/components/UI/HomePage/Specialist/Specialist';
 import React from 'react';
 
 const HomePage = () => {
   return (
     <>
       <HeroSection/>
+      <Specialist/>
     </>
   );
 };
