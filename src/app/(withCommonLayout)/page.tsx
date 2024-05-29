f994ad27ae50c723d758f4baf77c1f54ef0ff14e
junnun15 <junnunprodhan@gmail.com>
2024-05-30 01:44:19 +0600

    added why choose us section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,7 @@
 
 import HeroSection from '@/components/UI/HomePage/HeroSection/HeroSection';
 import Specialist from '@/components/UI/HomePage/Specialist/Specialist';
+import WhyUs from '@/components/UI/HomePage/WhyUs/WhyUs';
 import React from 'react';
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
     <>
       <HeroSection/>
       <Specialist/>
+      <WhyUs/>
     </>
   );
 };
