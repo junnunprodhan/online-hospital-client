948afb71aa6694b2ce1095671304dff470d8b539
junnun15 <junnunprodhan@gmail.com>
2024-05-31 00:07:19 +0600

    add zod validation

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,31 +1,158 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import Link from "next/link";
+'use client';
 
+
+import useUserInfo from '@/hooks/useUserInfo';
+import { logoutUser } from '@/services/actions/logoutUser';
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
-    return (
-        <Container>
-            <Stack py={2}
-            direction='row'
-            justifyContent='space-between'
-            alignItems='center'>
-                <Typography variant="h5" component={Link} href='/' fontWeight={600}>
-                    O<Box component='span' color='primary.main' >
-                        n</Box>line Hospital
-                </Typography>
-                <Stack direction="row" justifyContent="space-between" gap={4}>
-          <Typography  component={Link} href='/consultation'>
-            Consultation
-          </Typography>
-          <Typography>Health Plans</Typography>
-          <Typography>Medicine</Typography>
-          <Typography>Diagnostics</Typography>
-          <Typography>NGOs</Typography>
-        </Stack>
-        <Button component={Link} href='/login'>Login</Button>
-            </Stack>
-        </Container>
+    const AuthButton =dynamic(
+        ()=> import("@/components/UI/AuthButton/AuthButton"),
+        {ssr:false}
     );
+   const userInfo = useUserInfo();
+   const router = useRouter();
+
+  
+
+   return (
+      <Box
+         sx={{
+            bgcolor: 'primary.main',
+         }}
+      >
+         <Container>
+            <Stack
+               py={2}
+               direction='row'
+               justifyContent='space-between'
+               alignItems='center'
+            >
+               <Typography
+                  variant='h4'
+                  component={Link}
+                  href='/'
+                  fontWeight={600}
+               >
+                  P
+                  <Box component='span' color='#ffffff'>
+                     H
+                  </Box>{' '}
+                  Health Care
+               </Typography>
+
+               <Stack direction='row' justifyContent='space-between' gap={4}>
+                  <Typography
+                     component={Link}
+                     href='/consultation'
+                     color='#ffffff'
+                  >
+                     Consultation
+                  </Typography>
+
+                  <Typography color='#ffffff'>Diagnostics</Typography>
+                  <Typography component={Link} href='/doctors' color='#ffffff'>
+                     Doctors
+                  </Typography>
+
+                  {userInfo?.userId ? (
+                     <Typography
+                        component={Link}
+                        href='/dashboard'
+                        color='#ffffff'
+                     >
+                        Dashboard
+                     </Typography>
+                  ) : null}
+               </Stack>
+
+               <AuthButton/>
+            </Stack>
+         </Container>
+      </Box>
+   );
 };
 
 export default Navbar;
+
+
+
+// 'use client';
+
+// import AuthButton from '@/components/UI/AuthButton/AuthButton';
+// import useUserInfo from '@/hooks/useUserInfo';
+// import { logoutUser } from '@/services/actions/logoutUser';
+// import { Box, Button, Container, Stack, Typography } from '@mui/material';
+// import Link from 'next/link';
+// import { useRouter } from 'next/navigation';
+
+// const Navbar = () => {
+//     const userInfo = useUserInfo();
+//    const router = useRouter();
+
+//    const handleLogOut = () => {
+//       logoutUser(router);
+//    };
+
+//    return (
+//       <Box
+//          sx={{
+//             bgcolor: 'primary.main',
+//          }}
+//       >
+//          <Container>
+//             <Stack
+//                py={2}
+//                direction='row'
+//                justifyContent='space-between'
+//                alignItems='center'
+//             >
+//                <Typography
+//                   variant='h4'
+//                   component={Link}
+//                   href='/'
+//                   fontWeight={600}
+//                >
+//                   P
+//                   <Box component='span' color='#ffffff'>
+//                      H
+//                   </Box>{' '}
+//                   Health Care
+//                </Typography>
+
+//                <Stack direction='row' justifyContent='space-between' gap={4}>
+//                   <Typography
+//                      component={Link}
+//                      href='/consultation'
+//                      color='#ffffff'
+//                   >
+//                      Consultation
+//                   </Typography>
+
+//                   <Typography color='#ffffff'>Diagnostics</Typography>
+//                   <Typography component={Link} href='/doctors' color='#ffffff'>
+//                      Doctors
+//                   </Typography>
+
+//                   {userInfo?.userId ? (
+//                      <Typography
+//                         component={Link}
+//                         href='/dashboard'
+//                         color='#ffffff'
+//                      >
+//                         Dashboard
+//                      </Typography>
+//                   ) : null}
+//                </Stack>
+
+//                <AuthButton/>
+//             </Stack>
+//          </Container>
+//       </Box>
+//    );
+// };
+
+// export default Navbar;
